fbae4d447b0953a7d09464368030b9b263c5ed1c
Joseph Kim <joseph.kim.kr@gmail.com>
2017-11-03 15:03:11 -0700

    edit routes

--- pyramid_journal/routes.py
+++ pyramid_journal/routes.py
@@ -5,8 +5,6 @@ def includeme(config):
     """Include the following routes for static files and uri paths."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    # config.add_route('detail', '/journal/{id:\d+}') this is regex
-    config.add_route('detail', '/journal/1')
+    config.add_route('detail', '/journal/{id:\d+}')
     config.add_route('new', '/journal/new-entry')
-    # config.add_route('detail', '/journal/{id:\d+}/edit-entry')
-    config.add_route('edit', '/journal/1/edit-entry')
+    config.add_route('detail', '/journal/{id:\d+}/edit-entry')
--- pyramid_journal/views/default.py
+++ pyramid_journal/views/default.py
@@ -1,33 +1,70 @@
 """Module with view functions that serve each uri."""
 
 
-from pyramid.response import Response
+# from pyramid.response import Response
+# import os
+
+# HERE = os.path.abspath(__file__)
+# TEMPLATES = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'templates')
+# DATA = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'data')
+
+
+# def list_view(request):
+#     """View for the home page."""
+#     with open(os.path.join(TEMPLATES, 'index.html')) as file:
+#         return Response(file.read())
+
+
+# def detail_view(request):
+#     """View for the detail page."""
+#     with open(os.path.join(DATA, 'mon_oct_30.html')) as file:
+#         return Response(file.read())
+
+
+# def create_view(request):
+#     """View for the create new entry page."""
+#     with open(os.path.join(TEMPLATES, 'new.html')) as file:
+#         return Response(file.read())
+
+
+# def update_view(request):
+#     """View for the edit page."""
+#     with open(os.path.join(TEMPLATES, 'edit.html')) as file:
+#         return Response(file.read())
+
+from pyramid.view import view_config
+from pyramid_journal.data.data import ENTRIES
+
 import os
 
-HERE = os.path.abspath(__file__)
-TEMPLATES = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'templates')
-DATA = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'data')
+HERE = os.path.dirname(__file__)
 
 
+@view_config(route_name='home', renderer="../templates/list_view.jinja2")
 def list_view(request):
-    """View for the home page."""
-    with open(os.path.join(TEMPLATES, 'index.html')) as file:
-        return Response(file.read())
+    """View config for list view."""
+    return {"entries": ENTRIES}
 
 
+@view_config(route_name="detail", renderer="../templates/detail_view.jinja2")
 def detail_view(request):
-    """View for the detail page."""
-    with open(os.path.join(DATA, 'mon_oct_30.html')) as file:
-        return Response(file.read())
+    """View config for detail view."""
+    the_id = int(request.matchdict['id'])
+    for entry in ENTRIES:
+        if entry['id'] == the_id:
+            return {"entry": entry}
 
 
+@view_config(route_name="create", renderer="../templates/create_view.jinja2")
 def create_view(request):
-    """View for the create new entry page."""
-    with open(os.path.join(TEMPLATES, 'new.html')) as file:
-        return Response(file.read())
+    """View config for create view."""
+    return {}
 
 
+@view_config(route_name="update", renderer="../templates/update_view.jinja2")
 def update_view(request):
-    """View for the edit page."""
-    with open(os.path.join(TEMPLATES, 'edit.html')) as file:
-        return Response(file.read())
+    """View config for update view."""
+    the_id = int(request.matcdict['id'])
+    for entry in ENTRIES:
+        if entry['id'] == the_id:
+            return {"entry": entry}
